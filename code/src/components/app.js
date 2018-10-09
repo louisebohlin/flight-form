93186e30126d4c8e1b4f7a66c2b7d61474173d54
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-10-09 14:13:02 +0200

    Final version

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,11 +1,47 @@
 import React from "react"
+import "./app.scss"
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        Find me in src/app.js!
+      <div className="header">
+      <img src="./logo.svg" />
+      </div>
+        <div className="hero">
+          <img src="./hero.jpg" alt="airplane" />
+          <div className="container">
+            <div className="search-div">
+              <div className="searchfield">
+                <h4>Flyg från</h4>
+                  <form>
+                  <div className="input-field">
+                  <label>
+                    <input type="text" placeholder="Var flyger du ifrån?"/>
+                  </label>
+                  </div>
+                </form>
+                <p>Stockholm-Arlanda?</p>
+              </div>
+              <div className="searchfield">
+                <h4>Flyg till</h4>
+                  <form>
+                  <div className="input-field">
+                  <label>
+                    <input type="text" placeholder="Vart vill du resa?"/>
+                  </label>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="bottom-div">
+              <div className="button">
+                <button><p>Sök och beställ</p></button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
